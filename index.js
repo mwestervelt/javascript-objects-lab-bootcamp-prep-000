bca52b8dd9e0bac591854018efe01e10afbb43fb
Mariel Westervelt <elle.westervelt@gmail.com>
2018-10-03 13:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ var recipes = {}
 function updateObjectWithKeyAndValue(obj, key, value) {
    return Object.assign({}, obj, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
+
+function deleteFromObjectByKey(obj, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key]
+  return obj
+}
